1709cbb46ab7c5f43a33903116d3c9481565d934
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-19 20:16:53 +0200

    Update

--- action-Snapcast.py
+++ action-Snapcast.py
@@ -44,23 +44,34 @@ def get_slots(data):
     return slot_dict
 
 
+def get_site_info(slot_dict, request_siteid):
+    site_info = {'err': None, 'room_name': None, 'site_id': None}
+    if 'room' in slot_dict:
+        if request_siteid in snapcast.site_info and \
+                slot_dict['room'] == snapcast.site_info[request_siteid]['room_name'] \
+                or slot_dict['room'] == "hier":
+            site_info['site_id'] = request_siteid
+        elif request_siteid in snapcast.site_info and \
+                slot_dict['room'] != snapcast.site_info[request_siteid]['room_name']:
+            dict_rooms = {snapcast.site_info[siteid]['room_name']: siteid for siteid in snapcast.site_info}
+            site_info['site_id'] = dict_rooms[slot_dict['room']]
+        else:
+            site_info['err'] = f"Der Raum {slot_dict['room']} wurde noch nicht konfiguriert."
+    else:
+        site_info['site_id'] = request_siteid
+    if 'room_name' in snapcast.site_info[site_info['site_id']]:
+        site_info['room_name'] = snapcast.site_info[site_info['site_id']]['room_name']
+    else:
+        site_info['err'] = f"Der Raum {slot_dict['room']} wurde noch nicht konfiguriert."
+    return site_info
+
+
 class Snapcast:
     def __init__(self):
-        self.site_devices = dict()
+        self.site_info = dict()
         self.site_music = dict()
         self.request_siteid = None
         self.threadobj_injection = None
-
-
-def thread_delayed_device_injection():
-    time.sleep(2)
-    all_names = list()
-    for site_id in snapcast.site_devices:
-        for name in snapcast.site_devices[site_id]:
-            if name not in all_names:
-                all_names.append(name)
-    inject(mqtt_client, 'audio_devices', all_names, snapcast.request_siteid, 'addFromVanilla')
-    snapcast.site_devices = dict()
 
 
 def thread_delayed_music_injection():
@@ -90,23 +101,31 @@ def thread_delayed_music_injection():
 def msg_ask_inject_devices(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
     snapcast.request_siteid = data['siteId']
-    client.publish('snapcast/request/allSites/siteMusic')
-    end_session(client, data['sessionId'], "Die Gerätenamen werden jetzt zu der lokalen Spracherkennung hinzugefügt.")
+    end_session(client, data['sessionId'], "Die Gerätenamen werden zur Spracherkennung hinzugefügt.")
+    all_names = list()
+    for site_id in snapcast.site_info:
+        for name in snapcast.site_info[site_id]['available_blt_devices']:
+            if name not in all_names:
+                all_names.append(name)
+        for name in snapcast.site_info[site_id]['conf_blt_devices']:
+            if name not in all_names:
+                all_names.append(name)
+        for name in snapcast.site_info[site_id]['conf_nonblt_devices']:
+            if name not in all_names:
+                all_names.append(name)
+    inject(client, 'audio_devices', all_names, snapcast.request_siteid, 'addFromVanilla')
 
 
-def msg_result_site_devices(client, userdata, msg):
+def msg_result_site_info(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
-    if not snapcast.site_devices:
-        snapcast.threadobj_injection = threading.Thread(target=thread_delayed_device_injection)
-        snapcast.threadobj_injection.start()
-    snapcast.site_devices[data['site_id']] = data['names']
+    snapcast.site_info[data['site_id']] = data
 
 
 def msg_ask_inject_music(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
     snapcast.request_siteid = data['siteId']
     client.publish('snapcast/request/allSites/siteMusic')
-    end_session(client, data['sessionId'], "Die Musik wird jetzt zur Spracherkennung hinzugefügt.")
+    end_session(client, data['sessionId'], "Die Musik wird zur Spracherkennung hinzugefügt.")
 
 
 def msg_result_site_music(client, userdata, msg):
@@ -125,6 +144,17 @@ def msg_injection_complete(client, userdata, msg):
 
 def msg_ask_play_music(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
+    site_info = get_site_info(get_slots(data), data['siteId'])
+    if site_info['err']:
+        end_session(client, data['sessionId'], site_info['err'])
+        return
+    client.publish(f'snapcast/request/oneSite/{site_info["site_id"]}/playMusic', json.dumps(get_slots(data)))
+    end_session(client, data['sessionId'])
+
+
+def msg_result_play_music(client, userdata, msg):
+    data = json.loads(msg.payload.decode("utf-8"))
+    notify(client, data['err'], data['siteId'])
 
 
 def end_session(client, session_id, text=None):
@@ -168,8 +198,9 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe('hermes/intent/' + add_prefix('snapcastMusicPlay'))
     client.subscribe('hermes/injection/complete')
 
-    client.message_callback_add('snapcast/answer/siteDevices', msg_result_site_devices)
+    client.message_callback_add('snapcast/answer/siteInfo', msg_result_site_info)
     client.message_callback_add('snapcast/answer/siteMusic', msg_result_site_music)
+    client.message_callback_add('snapcast/answer/playMusic', msg_result_play_music)
     client.subscribe('snapcast/answer/#')
 
 
@@ -189,4 +220,5 @@ if __name__ == "__main__":
     mqtt_client.on_connect = on_connect
     mqtt_client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
     mqtt_client.connect(MQTT_BROKER_ADDRESS.split(":")[0], int(MQTT_BROKER_ADDRESS.split(":")[1]))
+    mqtt_client.publish('snapcast/request/allSites/siteInfo')
     mqtt_client.loop_forever()

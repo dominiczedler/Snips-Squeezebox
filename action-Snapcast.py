2060876ad33ab0bac8053b268ebffd25736c8770
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-26 01:22:53 +0200

    Update

--- action-Snapcast.py
+++ action-Snapcast.py
@@ -6,7 +6,7 @@ import json
 import toml
 import configparser
 import uuid
-import LMSTools
+import lmscontroller
 
 
 USERNAME_INTENTS = "domi"
@@ -44,47 +44,21 @@ def get_slots(data):
     return slot_dict
 
 
-def get_site_info(slot_dict, request_siteid):
-    site_info = {'err': None, 'room_name': None, 'site_id': None}
-    if 'room' in slot_dict:
-        if request_siteid in squeezebox.sites_info and \
-                slot_dict['room'] == squeezebox.sites_info[request_siteid]['room_name'] \
-                or slot_dict['room'] == "hier":
-            site_info['site_id'] = request_siteid
-        elif request_siteid in squeezebox.sites_info and \
-                slot_dict['room'] != squeezebox.sites_info[request_siteid]['room_name']:
-            dict_rooms = {squeezebox.sites_info[siteid]['room_name']: siteid for siteid in squeezebox.sites_info}
-            site_info['site_id'] = dict_rooms[slot_dict['room']]
-        else:
-            site_info['err'] = f"Der Raum {slot_dict['room']} wurde noch nicht konfiguriert."
-    else:
-        site_info['site_id'] = request_siteid
-
-    if site_info['site_id'] in squeezebox.sites_info and 'room_name' in squeezebox.sites_info[site_info['site_id']]:
-        site_info['room_name'] = squeezebox.sites_info[site_info['site_id']]['room_name']
-    elif 'room' in slot_dict:
-        site_info['err'] = f"Der Raum {slot_dict['room']} wurde noch nicht konfiguriert."
-    else:
-        site_info['err'] = "Es gab einen Fehler."
-    return site_info
-
-
 class Squeezebox:
     def __init__(self):
         self.inject_requestids = dict()
-        self.sites_info = dict()
 
 
 def msg_result_site_info(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
-    squeezebox.sites_info[data['site_id']] = data
+    lmsctl.sites_info[data['site_id']] = data
 
 
 def msg_inject_names(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
     end_session(client, data['sessionId'])
 
-    err, all_names = lm_server.get_music_names()
+    err, all_names = lmsctl.get_music_names()
     if not err:
         operations = [('addFromVanilla', {'squeezebox_artists': all_names['artists']}),
                       ('addFromVanilla', {'squeezebox_albums': all_names['albums']}),
@@ -111,15 +85,20 @@ def msg_injection_complete(client, userdata, msg):
 def msg_music_new(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
     slot_dict = get_slots(data)
-    site_info = get_site_info(slot_dict, data['siteId'])
-    if site_info['err']:
-        end_session(client, data['sessionId'], site_info['err'])
-    else:
-        end_session(client, data['sessionId'])
-        player = lm_server.get_player_from_name(site_info['room_name'])
-        if player:
-            player.new_music(slot_dict.get('artist'), slot_dict.get('album'),
-                             slot_dict.get('title'), slot_dict.get('genre'))
+    err = lmsctl.new_music(slot_dict, data['siteId'])
+    end_session(client, data['sessionId'], err)
+
+
+def msg_result_bluetooth_connect(client, userdata, msg):
+    data = json.loads(msg.payload.decode("utf-8"))
+    if data['site_id'] in lmsctl.pending_actions and data['result']:
+        pending_action = lmsctl.pending_actions[data['site_id']]['pending_action']
+        if pending_action == "new_music":
+            slot_dict = lmsctl.pending_actions[data['site_id']]['slot_dict']
+            request_siteid = lmsctl.pending_actions[data['site_id']]['request_siteid']
+            err = lmsctl.new_music(slot_dict, request_siteid)
+            if err:
+                notify(mqtt_client, err, request_siteid)
 
 
 def end_session(client, session_id, text=None):
@@ -164,6 +143,9 @@ def on_connect(client, userdata, flags, rc):
     client.message_callback_add('squeezebox/answer/siteInfo', msg_result_site_info)
     client.subscribe('squeezebox/answer/#')
 
+    client.message_callback_add('bluetooth/answer/deviceConnect', msg_result_bluetooth_connect)
+    client.subscribe('bluetooth/answer/deviceConnect')
+
 
 if __name__ == "__main__":
     snips_config = toml.load('/etc/snips.toml')
@@ -184,7 +166,7 @@ if __name__ == "__main__":
         lms_host = "localhost"
         lms_port = 9000
 
-    lm_server = LMSTools.LMSServer(lms_host, lms_port)
+    lmsctl = lmscontroller.LMSController(lms_host, lms_port)
 
     squeezebox = Squeezebox()
 
---
+++ lmscontroller.py
@@ -0,0 +1,136 @@
+import LMSTools
+import requests
+
+
+class LMSController:
+    def __init__(self, lms_host, lms_port):
+        self.server = LMSTools.LMSServer(lms_host, lms_port)
+        self.sites_info = dict()
+        self.pending_actions = dict()
+
+    def get_music_names(self):
+        try:
+            all_titles = list()
+            titles = self.server.request(params="titles list")
+            if titles.get('count') >= 1:
+                for title_dict in titles.get('titles_loop'):
+                    title = title_dict['title']
+                    if title not in all_titles:
+                        all_titles.append(title)
+
+            all_albums = list()
+            albums = self.server.request(params="albums list")
+            if albums.get('count') >= 1:
+                for album_dict in albums.get('albums_loop'):
+                    album = album_dict['album']
+                    if album not in all_albums:
+                        all_albums.append(album)
+
+            all_artists = list()
+            artists = self.server.request(params="artists list")
+            if artists.get('count') >= 1:
+                for artist_dict in artists.get('artists_loop'):
+                    for artist in re.split(r'; |;|, |,', artist_dict['artist']):
+                        if artist and artist not in all_artists:
+                            all_artists.append(artist)
+
+            all_genres = list()
+            genres = self.server.request(params="genres list")
+            if genres.get('count') >= 1:
+                for genre_dict in genres.get('genres_loop'):
+                    for genre in re.split(r'; |;|, |,|/| / ', genre_dict['genre']):
+                        if genre and genre not in all_genres:
+                            all_genres.append(genre)
+
+            all_playlists = list()
+            playlists = self.server.request(params="playlists list")
+            if playlists.get('count') >= 1:
+                for playlist_dict in playlists.get('playlists_loop'):
+                    playlist = playlist_dict['playlist']
+                    if playlist not in all_playlists:
+                        all_playlists.append(playlist)
+
+            all_names = {
+                'titles': all_titles,
+                'albums': all_albums,
+                'artists': all_artists,
+                'genres': all_genres,
+                'playlists': all_playlists
+            }
+            return False, all_names
+
+        except requests.exceptions.ConnectionError:
+            return True, None
+
+    def get_site_info(self, slot_dict, request_siteid):
+        site_info = {'room_name': None, 'site_id': None}
+        err = None
+        if 'room' in slot_dict:
+            if request_siteid in self.sites_info and \
+                    slot_dict['room'] == self.sites_info[request_siteid]['room_name'] \
+                    or slot_dict['room'] == "hier":
+                site_info['site_id'] = request_siteid
+            elif request_siteid in self.sites_info and \
+                    slot_dict['room'] != self.sites_info[request_siteid]['room_name']:
+                dict_rooms = {self.sites_info[siteid]['room_name']: siteid for siteid in self.sites_info}
+                site_info['site_id'] = dict_rooms[slot_dict['room']]
+            else:
+                err = f"Der Raum {slot_dict['room']} wurde noch nicht konfiguriert."
+        else:
+            site_info['site_id'] = request_siteid
+
+        if site_info['site_id'] in self.sites_info and 'room_name' in self.sites_info[site_info['site_id']]:
+            site_info['room_name'] = self.sites_info[site_info['site_id']]['room_name']
+        elif 'room' in slot_dict:
+            err = f"Der Raum {slot_dict['room']} wurde noch nicht konfiguriert."
+        else:
+            err = "Es gab einen Fehler."
+        return err, site_info
+
+    def get_player(self, site_info):
+        player = self.server.get_player_from_name(site_info['room_name'])
+        if player:
+            return None, player
+        else:
+            return "Dieses Gerät gibt es nicht.", None
+
+    def new_music(self, slot_dict, request_siteid, pending_action=None):
+
+        err, site_info = self.get_site_info(slot_dict, request_siteid)
+        if err:
+            return err
+
+        if not pending_action:
+            self.pending_actions[site_info['site_id']] = {'pending_action': "new_music",
+                                                          'slot_dict': slot_dict,
+                                                          'request_siteid': request_siteid}
+            return None
+        else:
+            del self.pending_actions[site_info['site_id']]
+
+        err, player = self.get_player(site_info)
+        if err:
+            return err
+        try:
+            query_params = list()
+            artist = slot_dict.get('artist')
+            album = slot_dict.get('album')
+            title = slot_dict.get('title')
+            genre = slot_dict.get('genre')
+            if artist:
+                query_params.append(f"contributor.namesearch={'+'.join(artist.split(' '))}")
+            if album:
+                query_params.append(f"album.titlesearch={'+'.join(album.split(' '))}")
+            if title:
+                query_params.append(f"track.titlesearch={'+'.join(title.split(' '))}")
+            if genre:
+                query_params.append(f"genre.namesearch={'+'.join(genre.split(' '))}")
+            if not album and not title:
+                player.request(f"playlist shuffle 1")
+            else:
+                player.request(f"playlist shuffle 0")
+            player.request(f"playlist loadtracks {'&'.join(query_params)}")
+            return None
+
+        except requests.exceptions.ConnectionError:
+            return "Es konnte keine Verbindung zum Musik Server hergestellt werden."
